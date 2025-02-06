17e6226300194abbb6b42e58eb7db66ad5dc7db7
AbuSufiyan19 <abusufiyan8800@gmail.com>
2025-02-06 16:01:42 +0530

    bookings added and channges made

--- Client/src/components/renterViewAllBookings/renterViewAllBookings.jsx
+++ Client/src/components/renterViewAllBookings/renterViewAllBookings.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./RenterViewAllBookings.css"; 
+import "./renterViewAllBookings.css"; 
 import config from "../../utils/configurl";
 
 const RenterViewAllBookings = () => {
