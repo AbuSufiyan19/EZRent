30bee80189df7a99bd41f9904aecbe4e9ebdcd91
AbuSufiyan19 <abusufiyan8800@gmail.com>
2025-02-13 15:25:33 +0530

    NCF Model implementation

--- Server/Recommendation/app.py
+++ Server/Recommendation/app.py
@@ -132,4 +132,4 @@ def retrain():
     return "✅ Model retrained successfully."
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 6000)))
