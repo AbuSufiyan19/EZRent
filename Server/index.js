6b5a3446fee4b902362715ada99172ddb741d540
AbuSufiyan19 <abusufiyan8800@gmail.com>
2026-05-17 11:28:38 +0530

    DB changes

--- Server/index.js
+++ Server/index.js
@@ -40,7 +40,7 @@ app.use('/payment', paymentRoutes);
 
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
   })
