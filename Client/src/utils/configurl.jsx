13e51cb0d14d1afb289e913c6707e0c82d0b83cb
AbuSufiyan19 <abusufiyan8800@gmail.com>
2026-05-20 22:39:41 +0530

    code changes

--- Client/src/utils/configurl.jsx
+++ Client/src/utils/configurl.jsx
@@ -1,9 +1,9 @@
 // src/config.js
 const config = {
-    // BASE_API_URL: 'http://localhost:5000',
-    BASE_API_URL: 'https://ezrentalservicebackend.onrender.com',
-    // PY_API_URL: 'http://127.0.0.1:8000',
-    PY_API_URL: 'https://recommendequipments.onrender.com',
+    BASE_API_URL: 'http://localhost:5000',
+    // BASE_API_URL: 'https://ezrentalservicebackend.onrender.com',
+    PY_API_URL: 'http://127.0.0.1:8000',
+    // PY_API_URL: 'https://recommendequipments.onrender.com',
     RP_API_KEY_ID: 'rzp_test_LhSRBuue41VLk6',
   };
   
--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -9,12 +9,24 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret_key";
 
 // Email transporter configuration
+// const transporter = nodemailer.createTransport({
+//   service: "Gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 // Generate a verification token
